41449ee420abec665c729ef9421a2994f071abf4
Artem Netepin <artnetepin@gmail.com>
2017-01-16 16:22:47 +0200

    first app finished

---
+++ ci_example/application/models/Customer_model.php
@@ -0,0 +1,8 @@
+<?php
+class Customer_model extends CI_model{
+  public function getCustomers() {
+    $this->db->select('fname,lname');
+    $this->db->from('Customers');
+    return $this->db->get()->result_array();
+  }
+}
